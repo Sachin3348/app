b226e6a3faed0aff535a7b71ad693a233f3c8fce
sachin <sachin.mahto@functionup.org>
2022-12-13 20:10:55 +0530

    intial commit

---
+++ src/components/page.js
@@ -0,0 +1,24 @@
+// import react from 'react'
+
+function Page(){
+    return(
+        <div>
+
+            <header></header>
+            <ol>
+                <li>
+                    react is awesome 
+                </li>
+
+                <li>
+                    react is fun
+                </li>
+            </ol>
+
+            <footer></footer>
+        </div>
+    )
+}
+
+
+export default Page
